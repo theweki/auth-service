72fa6d110ae76c94524c72da29878adc413013bd
theweki <rp.weki.24@gmail.com>
2024-09-26 16:11:43 +0530

    Login Response

--- src/main/java/dev/weki/auth_service/dto/TokenRecord.java
+++ src/main/java/dev/weki/auth_service/dto/TokenRecord.java
@@ -1,6 +1,7 @@
 package dev.weki.auth_service.dto;
 
 public record TokenRecord(
+        UserRecord user,
         String token_type,
         String access_token
 ) {
--- src/main/java/dev/weki/auth_service/service/AuthService.java
+++ src/main/java/dev/weki/auth_service/service/AuthService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -49,8 +50,12 @@ public class AuthService {
         );
 
         Authentication authentication = authenticationManager.authenticate(token);
-
         String accessToken = tokenService.generateToken(authentication);
-        return new TokenRecord("Bearer", accessToken);
+
+        UserEntity user = userRepository
+                .findByEmail(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new TokenRecord(userMapper.toUserRecord(user), "Bearer", accessToken);
     }
 }
